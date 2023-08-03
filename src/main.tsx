c2348b588735d0424594d421b7a1c48c0caa1aba
AmelianceSkyMusic <AmelianceSkyMusic@gmail.com>
2023-08-03 20:22:17 +0300

    = import styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,6 @@ import '@fontsource/montserrat/300.css';
 import '@fontsource/montserrat/400.css';
 import '@fontsource/montserrat/700.css';
 
-import './index.scss';
+import './styles.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
