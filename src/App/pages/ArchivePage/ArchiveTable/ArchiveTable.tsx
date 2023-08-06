8166cb0c9fdddb568a718b1a205a5aa1bcc463c0
AmelianceSkyMusic <AmelianceSkyMusic@gmail.com>
2023-08-06 23:29:55 +0300

    ^ update ArchiveTable - add functions callback - add cell width

--- src/App/pages/ArchivePage/ArchiveTable/ArchiveTable.tsx
+++ src/App/pages/ArchivePage/ArchiveTable/ArchiveTable.tsx
@@ -3,16 +3,22 @@ import { Table } from '~components/Table/Table';
 import { TableButton } from '~components/TableButton/TableButton';
 import { CATEGORIES } from '~constants/CATEGORIES';
 import { getObjKeyByValue } from '~helpers/getObjKeyByValue';
+import { mapNotesData } from '~helpers/mapNotesData';
+import { useTypedDispatch } from '~store/hooks/useTypedDispatch';
 import { useTypedSelector } from '~store/hooks/useTypedSelector';
-import type { Note } from '~store/types/Note';
+import { notesSlice } from '~store/notes/notesSlice';
 
 export function ArchiveTable() {
 	const { archivedNotes } = useTypedSelector((state) => state.notesReducer);
+	const dispatch = useTypedDispatch();
+	const { actions } = notesSlice;
 
-	const columns: Column<Note>[] = [
+	const mappedDate = mapNotesData(archivedNotes);
+
+	const columns: Column<(typeof mappedDate)[number]>[] = [
 		{
 			key: 'icon',
-			header: '',
+			width: '32px',
 			cell: (row) => {
 				const category = getObjKeyByValue(CATEGORIES, row?.rowAllValues.category);
 				return <p className={`icon--${category}`} />;
@@ -21,43 +27,66 @@ export function ArchiveTable() {
 		{
 			key: 'name',
 			header: 'Name',
+			width: 2,
 		},
 		{
 			key: 'created',
 			header: 'Created',
+			width: 2,
 		},
 		{
 			key: 'category',
 			header: 'Category',
+			width: 2,
 		},
 		{
 			key: 'content',
 			header: 'Content',
+			width: 6,
 		},
 		{
 			key: 'dates',
 			header: 'Dates',
+			width: 1,
 		},
 		{
 			key: 'edit',
+			width: '48px',
 		},
 		{
 			key: 'to-archive',
-			header: () => <TableButton icon="icon--from-archive" />,
+			width: '48px',
+			header: () => (
+				<TableButton
+					icon="icon--from-archive"
+					onClick={() => dispatch(actions.moveAllNotesFromArchive())}
+				/>
+			),
 			cell: (row) => (
 				<TableButton
 					icon="icon--from-archive"
-					onClick={() => console.log(row.rowAllValues.id)}
+					onClick={() => dispatch(actions.moveNoteFromArchive(row.rowAllValues.id))}
 				/>
 			),
 		},
 		{
 			key: 'remove',
-			header: () => <TableButton icon="icon--trash" />,
+			width: '48px',
+			header: () => (
+				<TableButton icon="icon--trash" onClick={() => dispatch(actions.removeAllArchive())} />
+			),
 			cell: (row) => (
-				<TableButton icon="icon--trash" onClick={() => console.log(row.rowAllValues.id)} />
+				<TableButton
+					icon="icon--trash"
+					onClick={() => dispatch(actions.removeNoteFromArchive(row.rowAllValues.id))}
+				/>
 			),
 		},
 	];
-	return <Table columns={columns} data={archivedNotes} />;
+
+	return (
+		<>
+			<Table columns={columns} data={mappedDate} />
+		</>
+	);
 }
